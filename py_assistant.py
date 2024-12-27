3929e333f0c0372eff4227fb9d13975a54697f4b
35-hasib <hrrahman35@gmail.com>
2024-12-27 17:01:44 +0600

    Pt_assistant is ready

--- py_assistant.py
+++ py_assistant.py
@@ -1,31 +1,47 @@
 
-# set up 2 mode Text or speech
-# while loop
-# all condition
+# import library
+import webbrowser
+import wikipedia
+import smtplib
+import os
+import random
+import pygame
+import subprocess
 
-    # online
-    # google somthing_text
-    # youtube text
-    # wikipedia text 
-    # mail
-        # Enter mail
-        # Enter message
-    # codeforces
 
-    # ofline
-    # play music
-        # What is your mood today (good , sad, romantic)
-        # playing music............
 print('''Instruction :
     For Google : write 'google' space Something then prass enter
     For Youtube : write 'youtube' space Something then prass enter
     For Wikipedia : write 'wikipedia' space Somthing then prass enter
-    For sending mail : mail then entre, write 'mail' and press enter, then write message press enter to send mail.
+    For sending mail : write 'mail' then prass enter
     For playing music : write 'play music' then prass enter
     For exit the program : write 'stop' then prass enter
 ''')
     
 print('Your Program is running.........')
+
+
+pygame.mixer.init()
+AUDIO_FOLDER = 'D:\ZZ_Downloads\Music'
+POTPLAYER_PATH = 'C:\Program Files\DAUM\PotPlayer\PotPlayerMini64.exe'
+def list_files(folder, extensions):
+    return [f for f in os.listdir(folder) if f.endswith(extensions)]
+
+def play_random_file(folder, extensions, background=False):
+    files = list_files(folder, extensions)
+    if not files:
+        print(f"No files found in the folder: {folder}")
+    else:
+        file = os.path.join(folder, random.choice(files))
+        try:
+            if background:
+                subprocess.Popen([POTPLAYER_PATH, file])
+            else:
+                subprocess.run([POTPLAYER_PATH, file])
+            print(f'Playing file: {file}')
+        except Exception as e:
+            print(f"Error playing file: {e}")
+
 
 while True:
     n = input('Choose a mode 1 for speech 2 for text....!\n')
@@ -40,24 +56,45 @@ while True:
 # for text mode
 if n == '2':
     while True:
-        i = input('Enter your text : ')
-        i = i.lower()
-        if 'google' in i:
+        command= input('Enter your command: ')
+        command = command.lower()
+        if command.startswith('google '):
+            webbrowser.open('https://www.google.com/search?q='+command[7:])
             print('Search in Google.........')
-        elif 'youtube' in i:
+        elif command.startswith('youtube '):
+            webbrowser.open('https://www.youtube.com/results?search_query='+command[8:])
             print('Search in Youtube.........')
-        elif 'wikipedia' in i:
-            print('''Python is a high-level, general-purpose programming language. Its design philosophy emphasizes code readability with the use of significant indentation.''')
-        elif 'mail' in i:
-            mail = input('Enter mail : ')
+        elif command.startswith('wikipedia '):
+            query = command[len('wikipedia '):]
+            try:
+                summary = wikipedia.summary(query, sentences=1)
+                print(summary)
+            except wikipedia.exceptions.DisambiguationError as e:
+                print(f"Disambiguation error: {e.options}")
+            except wikipedia.exceptions.PageError:
+                print("No Wikipedia page found for the query.")
+
+        elif 'mail' in command:
+            server = smtplib.SMTP('smtp.gmail.com',587)
+            server.starttls()
+            smail = 'author.hasib@gmail.com'
+
+            rmail = input('Enter mail : ')
+            subject = input('Enter Subject : ')
             message = input('Enter Message :')
-            print('mail send...!')
-        elif 'codeforces' in i:
-            print('your mission is completed...!')
-        elif 'play music' in i:
-            mood = input('What is your mood today ? (good, sad or romantic)')
-            print('Playing music.....')
-        elif 'stop' in i:
+
+            text = f"Subject: {subject}\n\n{message}"
+            server.login(smail,'dqpjbkcokvarvujh')
+            server.sendmail(smail,rmail,text)
+
+            print('Mail sent successfully')
+
+        elif command.startswith('play music'):
+            play_random_file(AUDIO_FOLDER, ('.mp3', '.wav'), background=True)
+        # elif command.startswith('play video'):
+        #     play_random_file(VIDEO_FOLDER, ('.mp4', '.avi', '.mkv'))
+        
+        elif 'stop' in command:
             break
         else:
-            print("Don't understand !! Try again......")
+            print('Invalid input Try again....!')
